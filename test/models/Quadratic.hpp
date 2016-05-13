ad17072c391247e89170dc8134b10b725d0b848c
Markus Herb <mail@markusherb.de>
2016-05-13 22:06:21 +0200

    Add template-based quadratic test models

--- test/models/Quadratic.hpp
+++ test/models/Quadratic.hpp
@@ -55,6 +55,102 @@ public:
     }
 };
 
+template<template<typename> class StateType>
+class QuadraticTemplateSystemModel {
+public:
+
+    template<typename T, typename _T>
+    void predict(const StateType<T>& x, const StateType<_T>& u,
+                 StateType<T>& prediction) {
+        // return x.^2 + u
+        prediction = x.cwiseProduct(x) + u.template cast<T>();
+    }
+
+    template<typename T>
+    Covariance <T, StateType<T> > getCovariance()
+    {
+        return Covariance <T, StateType<T> >::Identity();
+    }
+};
+
+template<typename T, template<typename> class StateType>
+class QuadraticTemplateJacobianSystemModel {
+public:
+
+    void predict(const StateType<T>& x, const StateType<T>& u,
+                 StateType<T>& prediction)
+    {
+        // return x.^2 + u
+        prediction = x.cwiseProduct(x) + u;
+    }
+
+    Jacobian<T, StateType<T>> getJacobian(const StateType<T>& x,
+                                          const StateType<T>& u)
+    {
+        return 2. * x.asDiagonal();
+    }
+
+    Covariance <T, StateType<T> > getCovariance()
+    {
+        return Covariance<T, StateType<T> >::Identity();
+    }
+};
+
+template<template<typename> class StateType, template<typename> class MeasurementType>
+class QuadraticTemplateMeasurementModel {
+public:
+
+    template<typename T>
+    void measure(const StateType<T>& x, MeasurementType<T>& measurement) {
+        // return x.^2
+        measurement = x.cwiseProduct(x).template head<MeasurementType<T>::RowsAtCompileTime>();
+    }
+
+    template<typename T>
+    Covariance <T, MeasurementType<T> > getCovariance()
+    {
+        return Covariance<T, MeasurementType<T> >::Identity();
+    }
+};
+
+template<typename T, template<typename> class StateType, template<typename> class MeasurementType>
+class QuadraticTemplateJacobianMeasurementModel {
+public:
+    typedef StateType<T> State;
+    typedef MeasurementType<T> Measurement;
+
+    typedef Kalman::Jacobian<T, State, Measurement> Jacobian;
+public:
+    void measure(const State& x, Measurement& measurement) {
+        // return x.^2
+        measurement = x.cwiseProduct(x).template head<MeasurementType<T>::RowsAtCompileTime>();
+    }
+
+    Jacobian getJacobian(const StateType<T>& x)
+    {
+        return (2. * x.asDiagonal()).toDenseMatrix().template topLeftCorner<Measurement::RowsAtCompileTime, State::RowsAtCompileTime>();
+    }
+
+    Covariance <T, Measurement> getCovariance()
+    {
+        return Covariance<T, Measurement>::Identity();
+    }
+};
+
+template<typename T, template<typename> class StateType>
+class QuadraticCombinedJacobianSystemModel :
+    public QuadraticLinearizedSystemModel<StateType<T>>,
+    public QuadraticTemplateJacobianSystemModel<T, StateType>
+{
+};
+
+template<typename T, template<typename> class StateType, template<typename> class MeasurementType>
+class QuadraticCombinedJacobianMeasurementModel :
+    public QuadraticLinearizedMeasurementModel<StateType<T>, MeasurementType<T>>,
+    public QuadraticTemplateJacobianMeasurementModel<T, StateType, MeasurementType>
+{
+};
+
 } // namespace Models
 } // namespace Test
 } // namespace Kalman
