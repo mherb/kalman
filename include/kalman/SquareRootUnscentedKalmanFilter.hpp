7973005eee97e9aa06d69e9e6373ea00f140792d
Markus Herb <mail@markusherb.de>
2017-12-16 16:24:45 +0100

    Remove sigma point calc from UKF update (fixes #3)
    For the update step, the sigma points computed in the preceding prediction step are used and not re-calculated. Thanks to zy041984 for pointing this out!

--- include/kalman/SquareRootUnscentedKalmanFilter.hpp
+++ include/kalman/SquareRootUnscentedKalmanFilter.hpp
@@ -157,9 +157,6 @@ namespace Kalman {
         {
             SigmaPoints<Measurement> sigmaMeasurementPoints;
             
-            // Compute sigma points (using predicted state)
-            computeSigmaPoints();
-            
             // Predict measurement (and corresponding sigma points)
             Measurement y = this->template computeMeasurementPrediction<Measurement, CovarianceBase>(m, sigmaMeasurementPoints);
             
--- include/kalman/UnscentedKalmanFilter.hpp
+++ include/kalman/UnscentedKalmanFilter.hpp
@@ -157,13 +157,6 @@ namespace Kalman {
         {
             SigmaPoints<Measurement> sigmaMeasurementPoints;
             
-            // Compute sigma points (using predicted state)
-            if(!computeSigmaPoints())
-            {
-                // TODO: handle numerical error
-                assert(false);
-            }
-            
             // Predict measurement (and corresponding sigma points)
             Measurement y = this->template computeMeasurementPrediction<Measurement, CovarianceBase>(m, sigmaMeasurementPoints);
             
