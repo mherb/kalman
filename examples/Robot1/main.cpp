228637b0ba4d9051e2f1876946f594e34dd3474a
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2018-08-02 22:50:10 +0200

    reorder cmath and define for stability

--- examples/Robot1/main.cpp
+++ examples/Robot1/main.cpp
@@ -1,3 +1,10 @@
+
+// this MUST be first, otherwise there might be problems on windows
+// see: https://stackoverflow.com/questions/6563810/m-pi-works-with-math-h-but-not-with-cmath-in-visual-studio/6563891#6563891
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+
 #include "SystemModel.hpp"
 #include "OrientationMeasurementModel.hpp"
 #include "PositionMeasurementModel.hpp"
@@ -8,9 +15,6 @@
 #include <iostream>
 #include <random>
 #include <chrono>
-
-#define _USE_MATH_DEFINES
-#include <cmath>
 
 
 using namespace KalmanExamples;
